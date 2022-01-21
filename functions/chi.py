d44671e09501fcb119817f560dd805d8546bd8d1
SerkanGuldal <fizikimparatoru@gmail.com>
2022-01-21 10:49:13 +0100

    clean up 2

--- functions/chi.py
+++ functions/chi.py
@@ -1,6 +1,12 @@
 from data_importer import data
 from sklearn.feature_selection import chi2
 
+# EXAMPLE
+# file_name = 'yeast3_label_class.csv' # Filename needs to be updated!!!!
+# NoV = 8 # Number of variables needs to be updated!!!!
+# data(file_name, NoV)
+
+
 d = data('yeast3_label_class.csv', 8)
 df = d[0]  # All dataset
 X  = d[1]  # features

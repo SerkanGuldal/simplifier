58f4cad3800176212cd96f9519bc6cee165c5439
Serkan Güldal <fizikimparatoru@gmail.com>
2023-07-29 16:56:42 +0300

    regular update

--- tools.py
+++ tools.py
@@ -6,13 +6,4 @@ def data(inputFile): # Data importer function
     NumberOfVariables = NumberOfColumns - 1 # The last colum is the label/class
     X = df.values[:,0:NumberOfVariables]
     y = df.values[:,NumberOfVariables]
-=======
-def data(inputFile): # Data importer function
-    file = open(os.path.dirname(__file__) + '/../datasets/' + inputFile)
-    df=pd.read_csv(file)
-    NumberOfColumns = len(df.columns)
-    NumberOfVariables = NumberOfColumns - 1 # The last colum is the label/class
-    X = df.values[:,0:NumberOfVariables]
-    y = df.values[:,NumberOfVariables]
->>>>>>> a31df81993c68bae07102ab3cac8ef9d3f463a29
     return(X, y)
